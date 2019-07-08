7bdd555e1c9885b5a45382a5174c22f4b224b291
Saly Abbo <elmaroufa@gmail.com>
2019-07-08 06:50:47 +0100

    Finition des travaux sur le manager des membres

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -1,24 +1,29 @@
-//modal pour le filtre des membres
-
+//fonction pour les fenetres modals
 $(document).ready(function() {
   $("#button-filtre-membre").click(filtreModal);
   $("#supress-m").click(modalDelete);
   $("#auth-button").click(modalAutorisation)
-  $("#myclose,.close,#close-remove").click(()=> {
-  $("#corp-filtre,#corp-delete,#corp-auth").addClass("d-none")
-
-})    
+  $("#myclose,.close,#close-remove,#close-auth").click(()=> {
+  $("#corp-filtre,#corp-delete,#corp-auth,#close-auth").addClass("d-none")
+     })  
+//fonction pour le date picker
+ $("#datepicker").datepicker({
+  uiLibrary: 'bootstrap4'
+ })
 })
 //fonction afficher le modal
 const afficher = () => {
   $("#modal-filtre-membre").modal();
 }
+let cacher_suprimer = () => $("#close-remove").addClass("d-none")
 
 //fonction qui execute tous ce qui conserne l'affichage du filtre et son contenue
 var process_filtre = () => {
   afficher() 
   $("#titre-modal").text("Filtre des membres")
   $("#corp-filtre").removeClass('d-none')
+  $("#myclose").removeClass('d-none')
+  cacher_suprimer()
 }
 //fonction qui affiche le message de supression
 var process_delete = () => {
@@ -31,7 +36,9 @@ var process_delete = () => {
 var process_autorisation= () => {
   afficher() 
   $("#titre-modal").text("Autorisation modal")
-  $("#corp-auth").removeClass('d-none')
+  $("#corp-auth,#close-auth").removeClass('d-none')
+  $("#myclose").addClass('d-none')
+  cacher_suprimer()
 }
 var filtreModal = function() {
   try {
